812b4fc1f65333b3f0ef1ea82f1e7f0ce812a5c9
EMobilio <eymobilio@gmail.com>
2023-07-28 13:54:27 -0400

    updated Player class to handle offscreen movement and added fields for health and bullet system

--- player.py
+++ player.py
@@ -10,12 +10,15 @@ class Player(pygame.sprite.Sprite):
             and the current player image
         """
         super().__init__()
-        ship_frame_1 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "ship_frame_1.png")), (100, 100))
-        ship_frame_2 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "ship_frame_2.png")), (100, 100))
+        ship_frame_1 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "ship_frame_1.png")).convert_alpha(), (100, 100))
+        ship_frame_2 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "ship_frame_2.png")).convert_alpha(), (100, 100))
         self.ship_frames = [ship_frame_1, ship_frame_2]
         self.frame_index = 0
         self.image = self.ship_frames[self.frame_index]
         self.rect = self.image.get_rect(midbottom=(400, 880))
+        self.lives = 3
+        self.health = 100
+        self.bullets = 30
 
     def change_animation(self):
         """ Marginally increments the frame_index attribute, assigning it a value of 0 when 
@@ -29,13 +32,20 @@ class Player(pygame.sprite.Sprite):
         self.image = self.ship_frames[int(self.frame_index)]
 
     def handle_movement(self):
-        """ Checks the keys pressed by the user and updates the position of the Player accordingly """
+        """ Checks the keys pressed by the user and updates the position of the Player accordingly,
+            moving it to the other end of the screen if it goes off screen in either direction 
+        """
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
             self.rect.x -= self.VELOCITY
         if keys[pygame.K_RIGHT]:
             self.rect.x += self.VELOCITY
-            
+
+        # Check for off screen movement and update the posiion accordingly
+        if self.rect.x <= -100:
+            self.rect.x = 800
+        elif self.rect.x >= 800:
+            self.rect.x = -100         
 
     def update(self):
         """ Updates the state of the player by calling the change_animation() and handle_movement()
