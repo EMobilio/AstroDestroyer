59ec09826a5b4087f52e11cff1d610fefac60950
EMobilio <eymobilio@gmail.com>
2023-07-28 20:07:42 -0400

    added comments and masks to the Asteroid and Bullet classes for collision detection

--- asteroid.py
+++ asteroid.py
@@ -1,23 +1,33 @@
 import pygame, os, random
 
 class Asteroid(pygame.sprite.Sprite):
+    """ Class for Asteroid sprite objects that inherits from the pygame Sprite class """
 
     def __init__(self, velocity):
+        """ Input: velocity- int
+         
+            Initializer for Asteroid objects that randomly chooses an image, sets the rect, mask,
+            and the velocity with the given argument
+        """
         super().__init__()
         type = random.choice(["asteroid_1.png", "asteroid_2.png", "asteroid_3.png", "asteroid_4.png"])
         self.image = pygame.transform.scale(pygame.image.load(os.path.join("assets", type)).convert_alpha(), (100, 100))
         self.rect = self.image.get_rect()
-        self.rect.inflate_ip(0, -40)
+        self.mask = pygame.mask.from_surface(self.image)
         self.rect.bottomleft = (random.randint(0, 800 - self.rect.width), random.randint(-450, -50))
         self.velocity = velocity
 
     def handle_movement(self):
+        """ Adds the asteroid's velocity to its y position to move it down the screen """
         self.rect.y += self.velocity
 
     def destroy(self):
+        """ Destroys the Asteroid object """
         self.kill()
 
     def update(self):
+        """ Updates the state of the asteroids by calling the handle_movement() and destroy() methods as needed """
         self.handle_movement()
+        # destroy asteroids that go beyond the bottom of the screen
         if self.rect.y > 900:
             self.destroy()
--- bullet.py
+++ bullet.py
@@ -1,21 +1,32 @@
 import pygame, os
 
 class Bullet(pygame.sprite.Sprite):
+    """ Class for Bullet sprite objects that inherits from the pygame Sprite class """
 
     VELOCITY = 5
 
     def __init__(self, pos):
+        """ Input: pos- tuple of ints 
+        
+            Initializer for Bullet objects that sets the image, mask and rect of the object with the position
+            from the pos argument
+        """
         super().__init__()
-        self.image = pygame.image.load(os.path.join("assets", "bullet.png"))
+        self.image = pygame.image.load(os.path.join("assets", "bullet.png")).convert_alpha()
         self.rect = self.image.get_rect(midbottom=pos)
+        self.mask = pygame.mask.from_surface(self.image)
 
     def handle_movement(self):
+        """ Subtracts the bullets's velocity from its y position to move it up the screen """
         self.rect.y -= self.VELOCITY
 
     def destroy(self):
-        if self.rect.y < -15:
-            self.kill()
+        """ Destroys the Bullet object """
+        self.kill()
 
     def update(self):
+        """ Updates the state of the bullets by calling the handle_movement() and destroy() methods as needed """
         self.handle_movement()
-        self.destroy()
+        # destroy bullets that go beyond the top of the screen
+        if self.rect.y < -10:
+            self.destroy()
